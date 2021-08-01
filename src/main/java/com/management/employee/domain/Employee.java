c1452a809b63f1d161181692c55d16828fd4a489
Viksingh_99 <vikramadityascode@gmail.com>
2021-08-01 23:31:35 +0530

    updated employee_id to id

--- src/main/java/com/management/employee/domain/Employee.java
+++ src/main/java/com/management/employee/domain/Employee.java
@@ -1,42 +1,36 @@
 package com.management.employee.domain;
 
-import java.util.UUID;
+import java.sql.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.NonNull;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+
 @Data
+@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
 
-	public static enum Department {
-		ONE,TWO,THREE,FOUR
-	};
+	private Integer id;
 
-	@NotBlank
-	private int employee_id;
+	@NonNull private String first_name;
 
-	@NotBlank
-	@Size(min=3,message = "First name must be atleast 3 character long")
-	private String first_name;
+	@NonNull private String last_name;
 
-	@NotBlank
-	@Size(min=3,message = "Last name must be atleast 3 character long")
-	private String last_name;
+	@NonNull private Date date_of_birth;
 
-	@NotBlank
-	private String date_of_birth;
+	@NonNull private String email;
 
-	@NotBlank
-	private String email;
+	@NonNull private Integer department_id;
 
-	@NotNull
-	private Department department_id;
+	private Date created_at;
+	private Date updated_at;
 }
