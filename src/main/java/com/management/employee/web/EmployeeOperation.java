7a266b942fad692ae4c336da7e7fb4262a5c1103
Viksingh_99 <vikramadityascode@gmail.com>
2021-08-01 23:34:20 +0530

    added database access

--- src/main/java/com/management/employee/web/EmployeeOperation.java
+++ src/main/java/com/management/employee/web/EmployeeOperation.java
@@ -1,7 +1,11 @@
 package com.management.employee.web;
 
+import com.management.employee.domain.Department;
 import com.management.employee.domain.Selection;
+import com.management.employee.repository.DepartmentsRepository;
+import com.management.employee.repository.EmployeesRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import com.management.employee.domain.Employee;
 import org.springframework.stereotype.Controller;
@@ -9,6 +13,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,15 +23,33 @@ import java.util.List;
 @Controller
 @Slf4j
 public class EmployeeOperation {
+
+    private final EmployeesRepository employeesRepository;
+    private DepartmentsRepository departmentsRepository;
+
+    private List<Integer> departments;
+
+
+    @Autowired
+    public EmployeeOperation(EmployeesRepository employeesRepository,DepartmentsRepository departmentsRepository){
+        this.employeesRepository = employeesRepository;
+        this.departmentsRepository = departmentsRepository;
+    }
+
+
     @ModelAttribute
     public void addAttributes(Model model) {
-        model.addAttribute("Departments",Employee.Department.values());
+        if(departments == null){
+            departments = new ArrayList<Integer>();
+            departmentsRepository.findAll().forEach(i -> departments.add(i.getId()));
+        }
+        model.addAttribute("Departments",departments);
     }
 
     @GetMapping("/")
     public String employees(Model model) {
-        List<Employee> Employees = Arrays.asList(new Employee(123, "3", "r", "e", "e", Employee.Department.FOUR), new Employee(23, "3", "r", "e", "e", Employee.Department.FOUR), new Employee(3, "3", "r", "e", "e", Employee.Department.FOUR), new Employee(1234, "3", "r", "e", "e", Employee.Department.FOUR), new Employee(124, "3", "r", "e", "e", Employee.Department.FOUR));
-
+        List<Employee> Employees = new ArrayList<Employee>();
+        employeesRepository.findAll().forEach(i->Employees.add(i));
 
         model.addAttribute("List",Employees);
         model.addAttribute("Select", new Selection());
@@ -45,12 +68,16 @@ public class EmployeeOperation {
             return "add employee";
         }
         log.info(String.valueOf(emp));
+        employeesRepository.add(emp);
         return "redirect:/employees/";
     }
 
     @PostMapping("delete")
     public String deleteEmployee(Selection emp){
         log.info(emp.getSelection().toString());
+        for ( var x: emp.getSelection()){
+            employeesRepository.delete(Integer.valueOf(x));
+        }
         return "redirect:/employees/";
     }
 }
