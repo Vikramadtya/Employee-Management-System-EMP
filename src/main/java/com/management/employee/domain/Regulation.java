260aad0a58d744d6d8d8fabb4d4c049aa691f4fe
Viksingh_99 <vikramadityascode@gmail.com>
2021-08-02 22:19:26 +0530

    added the constructor with no argument

--- src/main/java/com/management/employee/domain/Regulation.java
+++ src/main/java/com/management/employee/domain/Regulation.java
@@ -2,12 +2,14 @@ package com.management.employee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import java.sql.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Regulation {
     @NonNull String type;
     private Date created_at;
