7383975252703fe1bc2a0923159d18a34a8e166b
Viksingh_99 <vikramadityascode@gmail.com>
2021-08-02 17:08:03 +0530

    changed field names to be more relevant

--- src/main/java/com/management/employee/domain/Compliance.java
+++ src/main/java/com/management/employee/domain/Compliance.java
@@ -1,17 +1,21 @@
 package com.management.employee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 import java.sql.Date;
 
+@Data
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class Compliance {
 
     int id;
-    @NonNull String regulation;
+    @NonNull String regulation_type;
     @NonNull String detail;
-    @NonNull int department;
+    @NonNull int department_id;
 
     private Date created_at;
     private Date updated_at;
